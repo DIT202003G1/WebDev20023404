acbb584ad7e4b78f1273e992b57a42bd54d2f5f1
Frankium <tis7bfrankie@gmail.com>
2021-01-28 20:13:56 +0800

    add message dictionary (aka language file) and registration error messages

---
+++ Web-Src/php-includes/message.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+
+$msg_server_admin = "Please report this to the website admin.";
+$msg_field_empty = "This field cannot be empty";
+
+$msg_register_errors_testRegistration = [
+	"SUCCESS",
+	"Database STMT Error",
+	"You have already submitted an registration request, Please wait for approval",
+	"You have already signed up! Click on Sign In to sign in with your ACMS account!",
+];
+
+$msg_register_velidation = [
+	"id" => [
+		"SUCCESS",
+		"Student ID should be in 8 Digit Numbers",
+		"Student ID should be in 8 Digit Numbers"
+	],
+	"name" => [
+		"SUCCESS",
+		"Name should not be exceeding 64 characters"
+	],
+	"course" => [
+		"SUCCESS",
+		"Unknown Course"
+	],
+	"intake" => [
+		"SUCCESS",
+		"Unknown Intake"
+	],
+	"password" => [
+		"SUCCESS",
+		"Password cannot contain your student ID",
+		"Password length has to be atleast 8",
+		"Password should contain atleast 1 Uppercase, Lowercase <strong>AND</strong> Special Characters each",
+		"Password does not match"
+	]
+];
