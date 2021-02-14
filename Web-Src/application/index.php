e5724ab643d7e26fe277f4cb29559a15789ef55f
Frankium <tis7bfrankie@gmail.com>
2021-02-14 16:53:15 +0800

    add student app front end design

---
+++ Web-Src/application/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<?php require "/opt/lampp/htdocs/php-includes/common-includes.inc.php" ?>
+<?php require "/opt/lampp/htdocs/php-includes/sessionUtils.inc.php" ?>
+
+<?php
+	sessionRedirectStudnetApp();
+?>
+
+<html>
+	<head>
+		<?= $includes_head ?>
+		<link rel="stylesheet" type="text/css" href="/application/app.css">
+		<link rel="stylesheet" type="text/css" href="/application/app_control.css">
+	</head>
+	<body class="layoutFlex box horizontal-reversed">
+		<div class="mainpanel">
+			<div class="appbar layoutFlex box horizontal">
+				<div class="search">
+					<input type="text" placeholder="Name / ID / Course / Intake" id="searchVal"/>
+				</div>
+				<div class="profile">
+					<img src=""/>
+				</div>
+			</div>
+		</div>
+		<div class="leftpanel">
+			<div class="noselect appInfo layoutFlex box horizontal">
+				<h1>ACMS Pro</h1>
+				<div class="version">Ver 1.0</div>
+			</div>
+			<div class="bookmarkList">
+				<h2 class="noselect">Bookmarked Users</h2>
+				<div class="listItem selected layoutFlex box horizontal">
+					<img class="profile" src="http://localhost/assets/feature_logos/privacy.svg">
+					<div class="title">
+						<h3>Lorem Ipsum</h3>
+						<span>Dolor Sit Amet Consectetur Adipiscing</span>
+					</div>
+				</div>
+				<div class="listItem layoutFlex box horizontal">
+					<img class="profile" src="http://localhost/assets/feature_logos/privacy.svg">
+					<div class="title">
+						<h3>Lorem Ipsum</h3>
+						<span>Dolor Sit Amet</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?= $includes_foots ?>
+	</body>
+</html>
