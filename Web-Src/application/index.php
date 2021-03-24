b62ef8dc249355ffc79826df08817c4acd216420
Frankium <tis7bfrankie@gmail.com>
2021-03-24 18:39:47 +0800

    isolated left panel for student app

--- Web-Src/application/index.php
+++ Web-Src/application/index.php
@@ -25,29 +25,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="leftpanel">
-			<div class="noselect appInfo layoutFlex box horizontal">
-				<h1>ACMS Pro</h1>
-				<div class="version">Ver 1.0</div>
-			</div>
-			<div class="bookmarkList">
-				<h2 class="noselect">Bookmarked Users</h2>
-				<div class="listItem selected layoutFlex box horizontal">
-					<img class="profile" src="http://localhost/assets/feature_logos/privacy.svg">
-					<div class="title">
-						<h3>Lorem Ipsum</h3>
-						<span>Dolor Sit Amet Consectetur Adipiscing</span>
-					</div>
-				</div>
-				<div class="listItem layoutFlex box horizontal">
-					<img class="profile" src="http://localhost/assets/feature_logos/privacy.svg">
-					<div class="title">
-						<h3>Lorem Ipsum</h3>
-						<span>Dolor Sit Amet</span>
-					</div>
-				</div>
-			</div>
-		</div>
+		<?php require "/opt/lampp/htdocs/php-includes/appUtils/leftPanel.inc.php" ?>
 		<script type="text/javascript">
 			registerKeyPressEvent();
 		</script>
